da265be046d352a9dcac1ddb02116867d02063a9
mulesoft <mulesoft@BA-MULE23-OSX.local>
2018-11-13 03:57:24 -0300

    missing files added

---
+++ src/main/webapp/app/modules/validations/sotck.tsx
@@ -0,0 +1,101 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { Link, RouteComponentProps } from 'react-router-dom';
+import { Button, Col, Row, Table } from 'reactstrap';
+// tslint:disable-next-line:no-unused-variable
+import { ICrudGetAllAction, TextFormat } from 'react-jhipster';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+import { IRootState } from 'app/shared/reducers';
+// import { getEntities } from './purchase-order.reducer';
+import { IPurchaseOrder } from 'app/shared/model/purchase-order.model';
+import { getEntities as getProducts } from 'app/entities/product/product.reducer';
+import { getEntities as getPurchaseOrder } from 'app/entities/purchase-order/purchase-order.reducer';
+// tslint:disable-next-line:no-unused-variable
+import { APP_DATE_FORMAT, APP_LOCAL_DATE_FORMAT } from 'app/config/constants';
+import { IProductInPurchaseOrder } from 'app/shared/model/product-in-purchase-order.model';
+
+// export interface IPurchaseOrderProps extends RouteComponentProps<{ url: string }> {}
+
+interface StockValidationProps extends StateProps, DispatchProps, RouteComponentProps<{ purchaseOrder: string }> {}
+
+export class StockValidation extends React.Component<StockValidationProps> {
+  componentDidMount() {
+    // this.props.getEntities();
+
+    this.props.getProducts();
+    this.props.getPurchaseOrder();
+  }
+
+  render() {
+    // const { purchaseOrderList, match } = this.props;
+
+    const validateItemStock = (i: IProductInPurchaseOrder) => i.quantity < this.props.products.find(p => p.id == i.id).stock;
+
+    const passValidation = () => this.props.purchaseOrder.products.filter(p => validateItemStock(p)).length > 0;
+
+    const validationResult = () => {
+      if (passValidation()) {
+        return 'Exitosa';
+      } else {
+        return 'Fallida';
+      }
+    };
+
+    const formButton = () => {
+      if (passValidation()) {
+        return (
+          <Button className="float-right" tag={Link} to={`/validateStock`} color="info" size="sm">
+            <FontAwesomeIcon icon="check-square" /> <span className="d-none d-md-inline">Siguiente</span>
+          </Button>
+        );
+      } else {
+        return (
+          <Button className="float-right" tag={Link} to={`/validateStock`} color="info" size="sm">
+            <FontAwesomeIcon icon="check-square" /> <span className="d-none d-md-inline">Modificar</span>
+          </Button>
+        );
+      }
+    };
+
+    return (
+      <div>
+        <Row>
+          <Col>
+            <h2 id="purchase-order-heading">Validación de disponibilidad</h2>
+          </Col>
+        </Row>
+        <Row>
+          <Col xs="5" />
+          <Col xs="2">
+            <div>
+              <h4 className="text-center">Validacion: {validationResult()}</h4>
+            </div>
+          </Col>
+          <Col xs="5" />
+        </Row>
+        <Row>
+          <Col>{formButton()}</Col>
+        </Row>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (storeState: IRootState) => ({
+  products: storeState.product.entities,
+  purchaseOrder: storeState.purchaseOrder.entity
+});
+
+const mapDispatchToProps = {
+  getProducts,
+  getPurchaseOrder
+};
+
+type StateProps = ReturnType<typeof mapStateToProps>;
+type DispatchProps = typeof mapDispatchToProps;
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(StockValidation);
